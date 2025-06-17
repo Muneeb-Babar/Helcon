75265e020b14511c60d26a576422b1b5058bab37
Muneeb Baber <muneeb63ahmad63@gmail.com>
2025-06-17 15:54:28 +0500

    Customization

--- app/[locale]/(protected)/Registration/Location/MultiStepLocationForm.tsx
+++ app/[locale]/(protected)/Registration/Location/MultiStepLocationForm.tsx
@@ -49,7 +49,7 @@ export default function MultiStepLocationForm() {
   return (
     <div className="min-h-screen w-full flex flex-col lg:flex-row bg-[#f9fafb]">
       <aside className="w-full lg:w-1/4 bg-white shadow-md px-4 py-6 space-y-4">
-        <h1 className="text-xl font-bold mb-4">Client Onboarding</h1>
+        <h1 className="text-xl font-bold mb-4">Location</h1>
         <button onClick={() => setStep(0)} className={`block w-full text-left px-4 py-4 rounded-lg ${step === 0 ? "bg-yellow-300" : "bg-gray-100"}`}>Location Information</button>
         {/* <button onClick={() => setStep(1)} className={`block w-full text-left px-4 py-4 rounded-lg ${step === 1 ? "bg-yellow-300" : "bg-gray-100"}`}>Requested No. of Guards</button>
         <button onClick={() => setStep(2)} className={`block w-full text-left px-4 py-4 rounded-lg ${step === 2 ? "bg-yellow-300" : "bg-gray-100"}`}>For Accounts/Finance</button> */}
